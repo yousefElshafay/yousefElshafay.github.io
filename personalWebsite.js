68ebcfdce66ad050d3199649e97b94889a97e202
admin <admin@MacBook-Air-van-admin.local>
2020-05-17 06:13:40 +0200

    added collapsable functionality to mobile view

--- personalWebsite.js
+++ personalWebsite.js
@@ -93,3 +93,16 @@ sidenavBox.addEventListener('click', function(event) {
     sidebarBtn.style.display = "block";
 
 });
+
+
+// collapsible items 
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+
+    });
+}
